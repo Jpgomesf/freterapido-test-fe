246819bb74fd53463cf0593f6e89397d3976b926
jpgomesf <jppromus@gmail.com>
2023-10-15 15:15:25 -0300

    ✨ Implementa componentização e estiliza card, lista e toolbar.

--- src/app/currency-card/currency-card.component.ts
+++ src/app/currency-card/currency-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ConvertedCurrency, CurrencyService } from '../services/currency.service';
 
 @Component({
   selector: 'currency-card',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./currency-card.component.css']
 })
 export class CurrencyCardComponent {
+  @Input() currencyData: any
+  @Input() title: string = '';
+  @Input() error: boolean = false;
+  @Input() loading: boolean = false;
 
+
+  constructor(public currencyService: CurrencyService) { }
+
+  getColor(value: string) {
+    const numericValue = parseFloat(value);
+    if (numericValue <= 1.00) {
+      return { color: '#DD4B4B' };
+    } else if (numericValue <= 5.00) {
+      return { color: '#3C7649' };
+    } else {
+      return { color: '#3684CB' };
+    }
+  }
 }
--- src/app/currency-list/currency-list.component.ts
+++ src/app/currency-list/currency-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConvertedCurrency, CurrencyService } from '../services/currency.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'currency-list',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./currency-list.component.css']
 })
 export class CurrencyListComponent {
+  data: ConvertedCurrency | null = null;
+  loadingState: { [key: string]: boolean } = {
+    CAD: true,
+    ARS: true,
+    GBP: true,
+  };
+  errorState: { [key: string]: boolean } = {
+    CAD: false,
+    ARS: false,
+    GBP: false,
+  };
+  private subscription: Subscription | undefined;
 
+  constructor(public currencyService: CurrencyService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.currencyService.data$.subscribe((data) => {
+      this.data = data;
+    });
+    this.currencyService.data$.subscribe((data) => {
+      this.loadingState = this.currencyService.loadingState;
+      this.errorState = this.currencyService.errorState;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
