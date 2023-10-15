68121eddb34ac165bfea19625b2d397fe757b997
jpgomesf <jppromus@gmail.com>
2023-10-15 15:13:14 -0300

    ✨ Adiciona service ao array de providers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CurrencyListComponent } from './currency-list/currency-list.component';
 import { CurrencyCardComponent } from './currency-card/currency-card.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CurrencyService } from './services/currency.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarComponent,
     CurrencyListComponent,
-    CurrencyCardComponent,
-    ToolbarComponent
+    CurrencyCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
